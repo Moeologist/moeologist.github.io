8b324c5e775080799a3d1c4ceee7c10026d265e1
ala <moeala@outlook.com>
2020-10-15 21:36:07 +0800

    Site updated: 2020-10-15 21:36:07

--- js/knapsack.js
+++ js/knapsack.js
@@ -1,30 +1,12 @@
-function calData(w, W) {
-    var s = w.reduce((a, b) => a + b)
-    var cw = w.map((sum => value => sum += value)(0))
-    // var cw = w.reduce((a, x, i) => [...a, x + (a[i - 1] || 0)], [])
-    var ww = cw.map(x => x - s + W)
-    var xw = ww.map((x, i) => x > w[i] ? x : w[i])
-    var lo = xw.map(x => x - 1)
-    var dt = lo.map(x => W - x)
+function createConfig(w, W) {
+    let s = w.reduce((a, b) => a + b)
+    let cw = w.map((sum => value => sum += value)(0))
+    // let cw = w.reduce((a, x, i) => [...a, x + (a[i - 1] || 0)], [])
+    let ww = cw.map(x => x - s + W)
+    let xw = ww.map((x, i) => x > w[i] ? x : w[i])
+    let lo = xw.map(x => x - 1)
+    let dt = lo.map(x => W - x)
     return {
-        ww: ww,
-        xw: xw,
-        lo: lo,
-        dt: dt
-    }
-}
-
-function createChart(divID, w, W) {
-    var div = document.getElementById(divID)
-    var canvas = document.createElement('canvas')
-    div.appendChild(canvas)
-    canvas.setAttribute('width', 400)
-    canvas.setAttribute('height', 400)
-    var ctx = canvas.getContext('2d')
-
-    var C = calData(w, W)
-
-    var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
             labels: Array.from({ length: w.length }, (x, i) => i + 1).map(x => "i = " + x),
@@ -44,7 +26,7 @@ function createChart(divID, w, W) {
                     label: 'W - ∑',
                     backgroundColor: 'rgba(54, 32, 255, 1)',
                     borderColor: 'rgba(54, 32, 255, 1)',
-                    data: C.ww,
+                    data: ww,
                     borderWidth: 1,
                     fill: false,
                     type: 'line',
@@ -55,7 +37,7 @@ function createChart(divID, w, W) {
                     label: '下界改进',
                     backgroundColor: 'rgba(54, 32, 255, 0.1)',
                     borderColor: 'rgba(54, 32, 255, 0)',
-                    data: C.xw,
+                    data: xw,
                     borderWidth: 0,
                     fill: 0,
                     type: 'line',
@@ -64,7 +46,7 @@ function createChart(divID, w, W) {
                 },
                 {
                     label: '省去的计算',
-                    data: C.lo,
+                    data: lo,
                     borderWidth: 1,
                     categoryPercentage: 1,
                     barPercentage: 1
@@ -73,7 +55,7 @@ function createChart(divID, w, W) {
                     label: '必需的计算',
                     backgroundColor: 'rgba(88, 255, 47, 0.2)',
                     borderColor: 'rgba(88, 255, 47, 0.2)',
-                    data: C.dt,
+                    data: dt,
                     borderWidth: 1,
                     categoryPercentage: 1,
                     barPercentage: 1
@@ -122,47 +104,49 @@ function createChart(divID, w, W) {
                 ]
             }
         }
-    })
-    return myChart
+    }
 }
 
-{
-
-let w = [2, 4, 2, 1, 3, 5, 2, 1, 5, 2, 1, 4, 1]
-let W = 13
-
-let inputW = document.getElementById('input_W')
-let inputwi = document.getElementById('input_wi')
-
-inputW.value = W.toString()
-inputwi.value = w.reduce((a, b) => a + ' ' + b.toString())
-
-chart = createChart('chart', w, W)
-function updateChart() {
-    let w = inputwi.value.split(' ').map(x => parseInt(x))
-    let W = parseInt(inputW.value)
-    C = calData(w, W)
-    chart.data.labels = Array.from({ length: w.length }, (x, i) => i + 1).map(x => "i = " + x),
-    chart.data.datasets[0].data = w
-    chart.data.datasets[1].data = C.ww
-    chart.data.datasets[2].data = C.xw
-    chart.data.datasets[3].data = C.lo
-    chart.data.datasets[4].data = C.dt
-    chart.options.scales.yAxes[0].ticks.max = W
-    chart.options.scales.yAxes[1].ticks.max = W
-    chart.update()
+function createChart(divID, w, W) {
+    let div = document.getElementById(divID)
+    let canvas = document.createElement('canvas')
+    div.appendChild(canvas)
+    canvas.setAttribute('width', 400)
+    canvas.setAttribute('height', 400)
+    return new Chart(canvas.getContext('2d'), createConfig(w, W))
 }
 
-// inputW.onchange = updateChart
-// inputwi.onchange = updateChart
-document.getElementById('sort').onclick = () => {
-    this.his = inputwi.value.split(' ').map(x => parseInt(x))
-    inputwi.value = this.his.slice().sort((a, b) => a - b).reverse().reduce((a, b) => a + ' ' + b.toString())
-    updateChart()
-}
+window.onload = () => {
 
-document.getElementById('refresh').onclick = () => {
-    updateChart()
-}
+    let w = [2, 4, 2, 1, 3, 5, 2, 1, 5, 2, 1, 4, 1]
+    let W = 13
+
+    let inputW = document.getElementById('input_W')
+    let inputwi = document.getElementById('input_wi')
+
+    inputW.value = W.toString()
+    inputwi.value = w.reduce((a, b) => a + ' ' + b.toString())
+
+    chart = createChart('chart', w, W)
+    function updateChart() {
+        let w = inputwi.value.split(' ').map(x => parseInt(x))
+        let W = parseInt(inputW.value)
+        let conf = createConfig(w, W)
+        chart.data = conf.data
+        chart.options = conf.options
+        chart.update()
+    }
+
+    // inputW.onchange = updateChart
+    // inputwi.onchange = updateChart
+    document.getElementById('sort').onclick = () => {
+        this.his = inputwi.value.split(' ').map(x => parseInt(x))
+        inputwi.value = this.his.slice().sort((a, b) => a - b).reverse().reduce((a, b) => a + ' ' + b.toString())
+        updateChart()
+    }
+
+    document.getElementById('refresh').onclick = () => {
+        updateChart()
+    }
 
 }
